79f3afd3324228c1da3cc765f34a947c1148e60a
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-02 17:42:15 -0600

    Added link to edit in ideas index page.

--- spec/features/ideas/IDEAS4_user_can_edit_existing_idea_spec.rb
+++ spec/features/ideas/IDEAS4_user_can_edit_existing_idea_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-describe 'A user visits the ideas#new page' do
-  describe 'to see a form to create the idea' do
-    it 'fills in the info and clicks submit' do
+describe 'A user wants to edit an idea' do
+  describe 'so they visit the ideas#show page' do
+    it 'and fill in the info and clicks submit' do
       idea = Idea.create!(title: 'First', body: 'Whoo an idea')
       visit idea_path(idea)
 
@@ -17,6 +17,30 @@ describe 'A user visits the ideas#new page' do
       expect(page).to have_content('Yay an idea')
       expect(page).to_not have_content(idea.title)
       expect(page).to_not have_content(idea.body)
+    end
+  end
+
+  describe 'so they visit the ideas#index' do
+    it 'fills in the info and clicks submit' do
+      idea1 = Idea.create!(title: 'Uno', body: 'What an idea')
+      Idea.create!(title: 'Dos', body: 'Titanic')
+      Idea.create!(title: 'Tres', body: 'bob thunder')
+
+      visit ideas_path
+
+      within(".idea-#{idea1.id}") do
+        find('.edit').click
+      end
+
+      fill_in 'idea[title]', with: 'Whoa'
+      fill_in 'idea[body]', with: 'Yay an idea'
+      click_on 'Submit'
+
+      expect(current_path).to eq(idea_path(idea1))
+      expect(page).to have_content('Whoa')
+      expect(page).to have_content('Yay an idea')
+      expect(page).to_not have_content(idea1.title)
+      expect(page).to_not have_content(idea1.body)
     end
   end
 end
