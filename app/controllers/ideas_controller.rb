39da48c270cdc10c583c4f7e4bc4822928737434
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-02 08:14:59 -0600

    Added idea show functionality.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,5 +1,15 @@
 class IdeasController < ApplicationController
+  before_action :set_idea, only: %i[show]
   def index
     @ideas = Idea.all
   end
+
+  def show
+  end
+
+  private
+
+    def set_idea
+      @idea = Idea.find(params[:id])
+    end
 end
