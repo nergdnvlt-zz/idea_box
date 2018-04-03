dc18f5ccc546531a6579a0327f936158a856614a
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-03 14:18:58 -0600

    Added images controller, index view, added title column, and routes.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,7 @@
 class Image < ApplicationRecord
+  validates :src, presence: true
+  validates :title, presence: true
+
   has_many :image_ideas
   has_many :ideas, through: :image_ideas
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180403142222) do
+ActiveRecord::Schema.define(version: 20180403200351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180403142222) do
     t.text "src"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
   add_foreign_key "ideas", "categories"
