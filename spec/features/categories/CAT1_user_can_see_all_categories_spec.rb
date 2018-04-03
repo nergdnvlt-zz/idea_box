20fc52f19e1efe9b5f25bca69b992647a7e6d8ae
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-02 19:30:34 -0600

    Categories show and index page.

--- spec/features/categories/CAT1_user_can_see_all_categories_spec.rb
+++ spec/features/categories/CAT1_user_can_see_all_categories_spec.rb
@@ -15,4 +15,20 @@ describe 'A user visits the categories#index page' do
       expect(page).to have_content(cat3.name)
     end
   end
+
+  describe 'to link to individual category by name' do
+    it 'shows that category' do
+      cat1 = Category.create!(name: 'Brilliant')
+      Category.create!(name: 'Less Brilliant')
+      Category.create!(name: 'Stupid')
+
+      visit categories_path
+      expect(current_path).to eq(categories_path)
+
+      click_link cat1.name
+
+      expect(current_path).to eq(category_path(cat1))
+      expect(page).to have_content(cat1.name)
+    end
+  end
 end
