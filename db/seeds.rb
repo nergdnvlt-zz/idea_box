22efc09966a32ce065853c1270f216559d8da79b
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-04 11:33:20 -0600

    Added correct and working seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ Image.destroy_all
 Category.destroy_all
 User.destroy_all
 
-USERNAMES = %w[Bob Odin Thor Freya Hela Valyrie]
+USERNAMES = %w[Odin Thor Freya Hela Valyrie]
 CATEGORIES = %w[Kowabunga Radical Ridiculous]
 IMAGES = ['https://orig00.deviantart.net/9221/f/2008/344/a/7/palafitico_by_andymumford.jpg',
           'https://t00.deviantart.net/B9Uj_GJead3Q_-iuk1bpWRG49M4=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/ba15/th/pre/f/2016/151/a/4/magoito_sunset_by_andymumford-da4hzy8.jpg',
@@ -17,7 +17,7 @@ IDEAS = %w[Unbelievable Rad Rufio Lime Pineapple Kiwi Iphone Mad Neff DC Chopper
 BODY = ['Love it', 'Flower child', 'Free love', 'Get em', 'Oh yeah']
 USERS = []
 CATEGORY_IDS = []
-
+# FULL_IDEAS = []
 
 USERNAMES.each_with_index do |username, index|
   USERS.push(index = User.create!(username: username, password: 'test'))
@@ -31,7 +31,24 @@ IMAGES.each do |image|
   Image.create!(src: image, title: TITLES.sample)
 end
 
-IDEAS.each do |idea|
-  idea = Idea.new(title: idea, body: BODY.sample, category_id: CATEGORY_IDS.sample, user_id: USERS.sample)
-  idea.save
-end
+# IDEAS.each do |idea|
+#   FULL_IDEAS.push(Idea.create!(title: idea, body: BODY.sample, category_id: CATEGORY_IDS.sample, user_id: USERS.sample))
+#   require 'pry'; binding.pry
+# end
+
+idea1 = Idea.create!(title: IDEAS[0], body: BODY.sample, category_id: CATEGORY_IDS[0].id, user_id: USERS[0].id)
+idea2 = Idea.create!(title: IDEAS[1], body: BODY.sample, category_id: CATEGORY_IDS[1].id, user_id: USERS[1].id)
+idea3 = Idea.create!(title: IDEAS[2], body: BODY.sample, category_id: CATEGORY_IDS[2].id, user_id: USERS[2].id)
+idea4 = Idea.create!(title: IDEAS[3], body: BODY.sample, category_id: CATEGORY_IDS[0].id, user_id: USERS[3].id)
+idea5 = Idea.create!(title: IDEAS[4], body: BODY.sample, category_id: CATEGORY_IDS[1].id, user_id: USERS[4].id)
+idea6 = Idea.create!(title: IDEAS[5], body: BODY.sample, category_id: CATEGORY_IDS[2].id, user_id: USERS[0].id)
+idea7 = Idea.create!(title: IDEAS[6], body: BODY.sample, category_id: CATEGORY_IDS[0].id, user_id: USERS[1].id)
+idea8 = Idea.create!(title: IDEAS[7], body: BODY.sample, category_id: CATEGORY_IDS[1].id, user_id: USERS[2].id)
+idea9 = Idea.create!(title: IDEAS[8], body: BODY.sample, category_id: CATEGORY_IDS[2].id, user_id: USERS[3].id)
+idea10 = Idea.create!(title: IDEAS[9], body: BODY.sample, category_id: CATEGORY_IDS[0].id, user_id: USERS[4].id)
+idea11 = Idea.create!(title: IDEAS[10], body: BODY.sample, category_id: CATEGORY_IDS[1].id, user_id: USERS[0].id)
+idea12 = Idea.create!(title: IDEAS[11], body: BODY.sample, category_id: CATEGORY_IDS[2].id, user_id: USERS[1].id)
+idea13 = Idea.create!(title: IDEAS[12], body: BODY.sample, category_id: CATEGORY_IDS[0].id, user_id: USERS[2].id)
+idea14 = Idea.create!(title: IDEAS[13], body: BODY.sample, category_id: CATEGORY_IDS[1].id, user_id: USERS[3].id)
+
+require 'pry'; binding.pry
