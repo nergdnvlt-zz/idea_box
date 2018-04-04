26f3098dea65a8d1fd0520f617450a45180bcbda
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-04 15:22:25 -0600

    Added authorization for categories.

--- spec/features/categories/CAT4_user_can_edit_categories_spec.rb
+++ spec/features/categories/CAT4_user_can_edit_categories_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 describe 'A user visits the categories#edit page' do
   describe 'by linking from the categories#index' do
     it 'can edit an existing category' do
+      admin = User.create(username: 'odin', password: 'test', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       cat1 = Category.create!(name: 'Brilliant')
       Category.create!(name: 'Less Brilliant')
 
@@ -24,6 +26,8 @@ describe 'A user visits the categories#edit page' do
 
   describe 'by linking from the category#show' do
     it 'can edit an existing category' do
+      admin = User.create(username: 'odin', password: 'test', role: 1)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       cat1 = Category.create!(name: 'Brilliant')
       Category.create!(name: 'Super Strange')
 
