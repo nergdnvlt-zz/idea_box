b7a8de067295f7f2b2eb19706e281400f04b6c58
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-03 20:37:38 -0600

    Ok forgot to finish.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :images, except: [:edit, :update]
   resources :users, only: [:new, :create, :show]
-
+  resources :sessions, only: [:destroy]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
