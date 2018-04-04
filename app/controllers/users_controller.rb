f8a6857f10313b2b77272348d268d5dc63c8e8b5
nergdvnlt <tylerlundgren@yahoo.com>
2018-04-03 19:11:45 -0600

    Finished creating user account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash.notice = 'Account Created!'
+      redirect_to user_path(@user)
+    else
+      flash.notice = 'Account not created, try again.'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
